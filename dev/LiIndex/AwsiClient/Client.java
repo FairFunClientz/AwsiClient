53d389f6727e5d35e9cc32f3b29557567121278d
LiIndexUwU <93841328+LiIndexUwU@users.noreply.github.com>
2021-12-27 23:56:30 +0100

    Create Client.java

---
+++ dev/LiIndex/AwsiClient/Client.java
@@ -0,0 +1,13 @@
+package dev.LiIndex.AwsiClient;
+
+import net.minecraft.Minecraft;
+
+public class Client {
+  public static final String MODID = "Awsi";
+  public static final String MODVER = "0.0.1";
+  public static String name;
+  public Minecraft mc = Minecraft.getMinecraft();
+  
+  public static void register(Setting... s) {
+  }
+}
